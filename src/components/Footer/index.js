f6c23e0b65636217d7f381381ca20f45cebce6f3
David Teixeira <divield@gmail.com>
2020-08-20 17:22:18 -0300

    Link novo no rodapé

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,6 +9,7 @@ function Footer() {
         <img src="https://fontmeme.com/permalink/200810/384a7e785407d51c8bf9753f8fa6fd0a.png" alt="Logo Alura" />
       </Link>
       <p>Acesse a <a href="https://radiotdeducacao.vercel.app/" target="blank">Rádio TD Educação</a></p>
+      <p>Participe do nosso <a href="https://chat.whatsapp.com/Lm84PL1toXG73lGKVRtVOr" target="blank">GRUPO DO WHATSAPP</a></p>
       <p>
         Orgulhosamente criado por David Teixeira durante a
         {' '}
